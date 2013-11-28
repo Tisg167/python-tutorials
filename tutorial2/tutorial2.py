9d3667dfc58d4e5694aed5778f8de906844e1e27
trevstanhope <tpstanhope@gmail.com>
2013-11-27 21:10:25 -0500

    big changes

--- tutorial2/tutorial2.py
+++ tutorial2/tutorial2.py
@@ -9,8 +9,6 @@ import ast # import a module with import
 import time as t # when importing you can rename a module to make typing easier
 
 # It's good practice to define global values before everything else
-RUN_TIME = 10
-
 """
 A class is a collection of functions.
 It works much the same as a module like math, numpy, or ast.
@@ -24,33 +22,54 @@ class Robot:
     It is the class itself, and is passed around within
     all functions in a class
     """
-    self.startup_time = t.time()
-    self.mode = 'starting'   
-    
-  # Do an action
-  def action(self, action=None):
-    print(self.time_stamp + ' Doing Something: ' + str(None))    
-    
-  # What is the time
-  def get_state(self):
-    self.current_time = t.time()
-    self.time_stamp = '[' + str(self.current_time - self.startup_time) + ']'
-    if self.current_time - self.startup_time > RUN_TIME:
-      self.mode = 'off'
+    self.start_time = t.time()
+    self.mode = 'BOOT'
+    self.task = {}
+
+  def display():
+    print('Hearbeat: ' + str(self.heartbeat))
+    print('Mode: ' + self.mode)
+    print('Task: ' + self.task)
+
+
+  # Ask for user input
+  def ask_for_task(self, task_type='NIL'):
+    # anything within a method/function is indented at least once
+    string = raw_input(task_type + ': ') # get string input
+    while not self.task:
+      try:
+        task = ast.literal_eval(string) # change type to int
+      except Exception as msg:
+        print('That is not a proper task.')
+        task = {}
     else:
-      self.mode = 'running'
-    print('\t' + self.mode) # \t is TAB
-    
-  # Turn off at end of trial
-  def shutdown(self):
+      return task
+        
+  # Do an action
+  def action(self, task):
+    print(self.time_stamp) + task    
+
+  # Refresh
+  def refresh(self):
+    self.current_time = t.time()
+    self.up_time = (self.current_time - self.start_time)
+
+  # Enter standby
+  def standby(self, timeout=0):
     print(self.time_stamp + ' Trial Run Complete')
-    self.mode = 'off'
+    self.mode = 'STANDBY'
+    
+  # Turn off completely
+  def off(self, timeout=0):
+    print(self.time_stamp + ' Trial Run Complete')
+    self.mode = 'OFF'
 
 # Main
 if __name__ == '__main__':
   root = Robot()
-  while not (root.mode == 'off'): # check if the self.mode variable is not 'off'
-    root.get_state()
-    root.action()
+  while not (root.mode == 'OFF'): # check if the self.mode variable is not 'off'
+    task = root.ask_for_task()
+    action = root.action(task)
+    #root.queue = root.queue_task(task)
   else:
-    root.shutdown()
+    root.off()
